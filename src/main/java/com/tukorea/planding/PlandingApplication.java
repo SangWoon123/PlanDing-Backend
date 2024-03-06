f96c9f904354fca9275dfc28ed1471680446d93c
SangWoon123 <alrax3574@naver.com>
2024-03-06 18:26:15 +0900

    프로젝트 생성

---
+++ src/main/java/com/tukorea/planding/PlandingApplication.java
@@ -0,0 +1,13 @@
+package com.tukorea.planding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlandingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlandingApplication.class, args);
+	}
+
+}
