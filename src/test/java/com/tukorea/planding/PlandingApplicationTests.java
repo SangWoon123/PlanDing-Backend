448b1f33de79c4779554b81f0ad9b628c2d291ee
SangWoon123 <alrax3574@naver.com>
2024-03-12 11:42:25 +0900

    chore: test파일

---
+++ src/test/java/com/tukorea/planding/PlandingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tukorea.planding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlandingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
