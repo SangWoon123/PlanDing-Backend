5670955acae5abf556d923b06cf545b44b483653
SangWoon123 <alrax3574@naver.com>
2024-03-14 13:27:22 +0900

    chore: gitignore수정

--- src/main/java/com/tukorea/planding/global/config/SwaggerConfig.java
+++ src/main/java/com/tukorea/planding/global/config/SwaggerConfig.java
@@ -8,11 +8,15 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public OpenAPI openAPI(){
         Server server=new Server();
@@ -39,4 +43,12 @@ public class SwaggerConfig {
                 .info(info);
 
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
 }
