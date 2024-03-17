6a467da9696ea44e9930c908c275ba5809df9d6a
SangWoon123 <alrax3574@naver.com>
2024-03-17 14:36:55 +0900

    fix: webconfig설정변경

--- src/main/java/com/tukorea/planding/global/config/WebConfigure.java
+++ src/main/java/com/tukorea/planding/global/config/WebConfigure.java
@@ -9,7 +9,7 @@ public class WebConfigure implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:5173","http://localhost:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .maxAge(3600);
