49c5e3d72767e9a3dad3880f44f39caff2f12848
SangWoon123 <alrax3574@naver.com>
2024-03-31 01:42:49 +0900

    feat: Group R(유저가 속한 그룹룸 가져오기) 추가

--- src/main/java/com/tukorea/planding/group/service/GroupRoomService.java
+++ src/main/java/com/tukorea/planding/group/service/GroupRoomService.java
@@ -1,42 +1,55 @@
 package com.tukorea.planding.group.service;
 
 import com.tukorea.planding.group.dao.GroupRoomRepository;
+import com.tukorea.planding.group.dao.UserGroupMembershipRepository;
 import com.tukorea.planding.group.domain.GroupRoom;
 import com.tukorea.planding.group.domain.UserGroupMembership;
-import com.tukorea.planding.group.dto.RequestGroupRoom;
+import com.tukorea.planding.group.dto.RequestCreateGroupRoom;
+import com.tukorea.planding.group.dto.RequestInviteGroupRoom;
 import com.tukorea.planding.group.dto.ResponseGroupRoom;
 import com.tukorea.planding.user.dao.UserRepository;
 import com.tukorea.planding.user.domain.User;
 import com.tukorea.planding.user.dto.UserInfo;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class GroupRoomService {
 
     private final UserRepository userRepository;
+    private final UserGroupMembershipRepository userGroupMembershipRepository;
     private final GroupRoomRepository groupRoomRepository;
 
-    public ResponseGroupRoom createGroupRoom(UserInfo userInfo) {
+    @Transactional
+    public ResponseGroupRoom createGroupRoom(UserInfo userInfo, RequestCreateGroupRoom createGroupRoom) {
         User user = userRepository.findByEmail(userInfo.getEmail())
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         GroupRoom newGroupRoom = GroupRoom.builder()
+                .title(createGroupRoom.getTitle())
                 .owner(user.getCode())
                 .build();
 
         newGroupRoom.addUser(user);
         GroupRoom savedGroupRoom = groupRoomRepository.save(newGroupRoom);
 
+        // 중간테이블에 유저, 그룹 정보 저장
+        userGroupMembershipRepository.saveAll(newGroupRoom.getGroupMemberships());
+
         return ResponseGroupRoom.from(savedGroupRoom);
     }
 
-    //    @Transactional
-    public ResponseGroupRoom inviteGroupRoom(UserInfo userInfo, RequestGroupRoom invitedUserInfo) {
-        RequestGroupRoom checking = RequestGroupRoom.checking(invitedUserInfo);
+    @Transactional
+    public ResponseGroupRoom inviteGroupRoom(UserInfo userInfo, RequestInviteGroupRoom invitedUserInfo) {
+        RequestInviteGroupRoom checking = RequestInviteGroupRoom.checking(invitedUserInfo);
 
         User invitingUser = userRepository.findByEmail(userInfo.getEmail())
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
@@ -44,7 +57,7 @@ public class GroupRoomService {
         GroupRoom groupRoom = groupRoomRepository.findByGroupCode(invitedUserInfo.getInviteGroupCode())
                 .orElseThrow(() -> new IllegalArgumentException("GroupRoom Not Found"));
 
-        validInvitePermission(groupRoom,invitingUser);
+        validInvitePermission(groupRoom, invitingUser);
 
         User invitedUser = findUserByRequest(checking);
         groupRoom.addUser(invitedUser);
@@ -52,7 +65,19 @@ public class GroupRoomService {
         return ResponseGroupRoom.from(groupRoom);
     }
 
-    private User findUserByRequest(RequestGroupRoom checking) {
+    // 유저가 속한 그룹룸 가져오기
+    public List<ResponseGroupRoom> getAllGroupRoomByUser(UserInfo userInfo) {
+        User user = userRepository.findByEmail(userInfo.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        List<GroupRoom> groupRooms = groupRoomRepository.findGroupRoomsByUserId(user.getId());
+
+        return groupRooms.stream()
+                .map(ResponseGroupRoom::from)
+                .collect(Collectors.toList());
+    }
+
+    private User findUserByRequest(RequestInviteGroupRoom checking) {
         if (checking.getUserCode() == null) {
             return userRepository.findByEmail(checking.getUserEmail())
                     .orElseThrow(() -> new UsernameNotFoundException("InvitedUser not found"));
@@ -61,7 +86,8 @@ public class GroupRoomService {
                     .orElseThrow(() -> new UsernameNotFoundException("InvitedUser not found"));
         }
     }
-    private void validInvitePermission(GroupRoom groupRoom, User invitingUser){
+
+    private void validInvitePermission(GroupRoom groupRoom, User invitingUser) {
         if (!groupRoom.getOwner().equals(invitingUser.getCode())) {
             throw new IllegalArgumentException("User does not have permission to invite this groupRoom");
         }
