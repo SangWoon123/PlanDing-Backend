095262baef6845f36ffbaec692a10244b4eaccf8
SangWoon123 <alrax3574@naver.com>
2024-03-31 01:44:50 +0900

    fix: GroupRoom Repository 유저가속한 그룹 가져오기

--- src/main/java/com/tukorea/planding/group/dao/UserGroupMembershipRepository.java
+++ src/main/java/com/tukorea/planding/group/dao/UserGroupMembershipRepository.java
@@ -1,10 +1,7 @@
 package com.tukorea.planding.group.dao;
 
 import com.tukorea.planding.group.domain.UserGroupMembership;
-import com.tukorea.planding.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface UserGroupMembershipRepository extends JpaRepository<UserGroupMembership,Long> {
 }
