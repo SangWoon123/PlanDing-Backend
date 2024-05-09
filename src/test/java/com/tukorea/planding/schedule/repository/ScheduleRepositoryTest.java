f908d7cfcc08f5d336e77e682478d8495912a6ab
SangWoon123 <alrax3574@naver.com>
2024-05-10 00:27:15 +0900

    test: 스케줄 테스트코드 단위테스트로 변경

---
+++ src/test/java/com/tukorea/planding/schedule/repository/ScheduleRepositoryTest.java
@@ -0,0 +1,101 @@
+package com.tukorea.planding.schedule.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.tukorea.planding.domain.schedule.entity.Schedule;
+import com.tukorea.planding.domain.schedule.repository.ScheduleRepository;
+import com.tukorea.planding.domain.schedule.repository.ScheduleRepositoryCustom;
+import com.tukorea.planding.domain.schedule.repository.ScheduleRepositoryCustomImpl;
+import com.tukorea.planding.domain.user.entity.User;
+import com.tukorea.planding.domain.user.repository.UserRepository;
+import com.tukorea.planding.global.oauth.details.Role;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@DataJpaTest
+@Transactional
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ScheduleRepositoryTest {
+
+    private final ScheduleRepositoryCustom repository;
+    private final UserRepository userRepository;
+    private final ScheduleRepository scheduleRepository;
+    private User testUser;
+
+    public ScheduleRepositoryTest(EntityManager entityManager, UserRepository userRepository, ScheduleRepository scheduleRepository) {
+        this.repository = new ScheduleRepositoryCustomImpl(new JPAQueryFactory(entityManager));
+        this.userRepository = userRepository;
+        this.scheduleRepository = scheduleRepository;
+    }
+
+
+    @BeforeEach
+    public void setUp() {
+        final String email = "test@google.com";
+        final String userCode = "#test";
+
+        User user = User.builder()
+                .userCode(userCode)
+                .email(email)
+                .role(Role.USER)
+                .build();
+
+        testUser = userRepository.save(user);
+    }
+
+    private Schedule createAndSaveSchedule(User user, LocalTime startTime, LocalTime endTime) {
+        return Schedule.builder()
+                .user(user)
+                .scheduleDate(LocalDate.now())
+                .title("title")
+                .content("content")
+                .startTime(startTime)
+                .endTime(endTime)
+                .build();
+    }
+
+    @Test
+    public void 겹치는스케줄_가져오기() {
+
+        List<Schedule> schedules = new ArrayList<>();
+
+        LocalTime startTime = LocalTime.of(7, 0);
+        LocalTime endTime = LocalTime.of(9, 0);
+        Schedule schedule1 = createAndSaveSchedule(testUser, startTime, endTime);
+
+        LocalTime startTime2 = LocalTime.of(9, 0);
+        LocalTime endTime2 = LocalTime.of(11, 0);
+        Schedule schedule2 = createAndSaveSchedule(testUser, startTime2, endTime2);
+
+
+        LocalTime startTime3 = LocalTime.of(9, 0);
+        LocalTime endTime3 = LocalTime.of(9, 30);
+        Schedule schedule3 = createAndSaveSchedule(testUser, startTime3, endTime3);
+
+        schedules.add(schedule1);
+        schedules.add(schedule2);
+        schedules.add(schedule3);
+
+        scheduleRepository.saveAll(schedules);
+
+        // when
+        List<Schedule> result = repository.findOverlapSchedules(testUser.getId(), LocalDate.now(), LocalTime.of(8, 0), LocalTime.of(10, 0));
+
+        // then
+        assertThat(result).isNotEmpty();
+        assertThat(result.size()).isEqualTo(3);
+    }
+
+}
--- src/test/java/com/tukorea/planding/schedule/service/ScheduleServiceTest.java
+++ src/test/java/com/tukorea/planding/schedule/service/ScheduleServiceTest.java
@@ -1,175 +1,125 @@
 package com.tukorea.planding.schedule.service;
 
-import com.tukorea.planding.domain.schedule.entity.ScheduleStatus;
-import com.tukorea.planding.domain.schedule.repository.ScheduleRepository;
-import com.tukorea.planding.domain.schedule.entity.Schedule;
 import com.tukorea.planding.domain.schedule.dto.ScheduleRequest;
 import com.tukorea.planding.domain.schedule.dto.ScheduleResponse;
+import com.tukorea.planding.domain.schedule.entity.Schedule;
 import com.tukorea.planding.domain.schedule.repository.ScheduleRepositoryCustomImpl;
+import com.tukorea.planding.domain.schedule.service.ScheduleQueryService;
 import com.tukorea.planding.domain.schedule.service.ScheduleService;
-import com.tukorea.planding.domain.user.mapper.UserMapper;
-import com.tukorea.planding.domain.user.repository.UserRepository;
+import com.tukorea.planding.domain.user.entity.SocialType;
 import com.tukorea.planding.domain.user.entity.User;
-import com.tukorea.planding.domain.user.dto.UserInfo;
-import com.tukorea.planding.global.error.BusinessException;
-import com.tukorea.planding.global.error.ErrorCode;
+import com.tukorea.planding.domain.user.mapper.UserMapper;
+import com.tukorea.planding.domain.user.service.UserQueryService;
 import com.tukorea.planding.global.oauth.details.Role;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+
+@ExtendWith(MockitoExtension.class)
 @Transactional
 class ScheduleServiceTest {
 
-    private static final String TEST_EMAIL = "test@";
-    private static final String TEST_TITLE = "Test Schedule";
-    private static final String TEST_CONTENT = "Test Content";
-    private static final LocalDate TEST_DATE = LocalDate.of(2024, 01, 02);
-    private static final LocalDate END_DATE = LocalDate.of(2024, 01, 03);
 
-
-    @Autowired
+    @InjectMocks
     private ScheduleService scheduleService;
-    @Autowired
-    private ScheduleRepository scheduleRepository;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private ScheduleRepositoryCustomImpl scheduleRepositoryCustom;
 
-    @Test
-    void createSchedule() {
-        //given
-        UserInfo userInfo = UserInfo.builder()
-                .email("test@")
-                .userCode("#test")
-                .role(Role.USER)
-                .build();
+    @Mock
+    UserQueryService userQueryService;
 
-        User user = User.builder()
-                .email("test@")
-                .userCode("#test")
-                .role(Role.USER)
-                .build();
+    @Mock
+    ScheduleRepositoryCustomImpl scheduleRepositoryCustomImpl;
 
-        userRepository.save(user);
+    @Mock
+    ScheduleQueryService scheduleQueryService;
 
-        LocalTime startTime = LocalTime.of(9, 0);
-        LocalTime endTime = LocalTime.of(10, 0);
 
-        ScheduleRequest scheduleRequest = ScheduleRequest.builder()
-                .startTime(startTime)
-                .endTime(endTime)
-                .title(TEST_TITLE)
-                .content(TEST_CONTENT)
-                .scheduleDate(TEST_DATE)
-                .build();
+    private User testUser;
+    private Schedule schedule;
+    private ScheduleRequest scheduleRequest;
 
-        //when
-        ScheduleResponse schedule = scheduleService.createSchedule(userInfo, scheduleRequest);
-
-        //then
-        assertNotNull(schedule);
-        assertEquals(TEST_TITLE, schedule.title());
-        assertEquals(TEST_CONTENT, schedule.content());
-        assertEquals(startTime, schedule.startTime());
-        assertEquals(endTime, schedule.endTime());
-    }
-
-    @Test
-    void deleteSchedule() {
-        //given
-        UserInfo userInfo = UserInfo.builder()
-                .email(TEST_EMAIL)
-                .userCode("#CODE")
-                .role(Role.USER)
-                .build();
-
-        createUserAndSave(TEST_EMAIL);
-
-        LocalTime startTime = LocalTime.of(9, 0);
-        LocalTime endTime = LocalTime.of(10, 0);
-
-        ScheduleRequest scheduleRequest = ScheduleRequest.builder()
-                .startTime(startTime)
-                .endTime(endTime)
-                .title(TEST_TITLE)
-                .content(TEST_CONTENT)
-                .scheduleDate(TEST_DATE)
-                .build();
-
-        //when
-        ScheduleResponse schedule = scheduleService.createSchedule(userInfo, scheduleRequest);
-        scheduleService.deleteSchedule(userInfo, schedule.id());
-
-        //then
-        assertEquals(Optional.empty(), scheduleRepository.findById(schedule.id()));
-    }
-
-    @Test
-    @DisplayName("주간 개인 스케줄 가져오기")
-    public void getWeekSchedule() {
-        //given
-        UserInfo userInfo = UserInfo.builder()
-                .email("test@")
-                .role(Role.USER)
-                .userCode("#CODE")
-                .username("username")
-                .build();
-
-        User user = createUserAndSave(TEST_EMAIL);
-
-        LocalTime startTime = LocalTime.of(9, 0);
-        LocalTime endTime = LocalTime.of(10, 0);
-
-        Schedule schedule = Schedule
-                .builder()
+    private Schedule createAndSaveSchedule(User user, LocalTime startTime, LocalTime endTime) {
+        return Schedule.builder()
                 .user(user)
-                .scheduleDate(TEST_DATE)
-                .title(TEST_TITLE)
-                .content(TEST_CONTENT)
+                .scheduleDate(LocalDate.now())
+                .title("title")
+                .content("content")
                 .startTime(startTime)
                 .endTime(endTime)
                 .build();
+    }
 
-        scheduleRepository.save(schedule);
+    @BeforeEach
+    void setUp() {
 
-        Schedule schedule1 = createAndSaveSchedule(user, TEST_TITLE, TEST_CONTENT, startTime, endTime, TEST_DATE);
+        testUser = new User("test", "profile", "username", Role.USER, SocialType.KAKAO, null, "#test"); // 테스트용 사용자 정보 초기화
+        schedule = new Schedule("title", "content", LocalDate.now(), LocalTime.of(9, 0), LocalTime.of(10, 0), true, null, testUser);
+        scheduleRequest = new ScheduleRequest("#test", "title", "content", LocalDate.now(), LocalTime.of(9, 0), LocalTime.of(10, 0));
+    }
 
-        scheduleRepository.save(schedule1);
+
+    @Test
+    void create_스케줄_생성() {
+        // given
+        when(scheduleQueryService.save(any(Schedule.class))).thenReturn(schedule);
+
+        // when
+        ScheduleResponse result = scheduleService.createSchedule(UserMapper.toUserInfo(testUser), scheduleRequest);
+
+        // then
+        assertNotNull(result);
+        assertEquals("title", result.title());
+        assertEquals("content", result.content());
+        assertEquals(LocalTime.of(9, 0), result.startTime());
+        assertEquals(LocalTime.of(10, 0), result.endTime());
+    }
+
+    @Test
+    void delete_스케줄_삭제() {
+        //given
+        when(scheduleQueryService.findScheduleById(1L)).thenReturn(schedule);
 
         //when
-        List<ScheduleResponse> result = scheduleService.getWeekSchedule(TEST_DATE, END_DATE, userInfo);
+        scheduleService.deleteSchedule(UserMapper.toUserInfo(testUser), 1L);
+
+        //then
+        verify(scheduleQueryService).delete(schedule);
+    }
+
+    @Test
+    public void 주간개인스케줄_가져오기() {
+        //given
+        List<Schedule> responses = Collections.singletonList(schedule);
+        when(userQueryService.getUserByUserCode(any())).thenReturn(testUser);
+        when(scheduleRepositoryCustomImpl.findWeeklyScheduleByUser(any(), any(), eq(testUser))).thenReturn(responses);
+
+        //when
+        List<ScheduleResponse> result = scheduleService.getWeekSchedule(LocalDate.now(), LocalDate.now().plusDays(7), UserMapper.toUserInfo(testUser));
 
         //then
         assertNotNull(result);
-        assertEquals(2, result.size());
-        assertEquals(result.get(0).startTime(), schedule1.getStartTime());
-        assertEquals(result.get(1).startTime(), schedule.getStartTime());
+        assertEquals(1, result.size());
+        assertEquals(result.get(0).startTime(), schedule.getStartTime());
+        assertEquals(result.get(0).endTime(), schedule.getEndTime());
     }
 
     @Test
-    @DisplayName("스케줄 수정 테스트")
-    public void updateSchedule() {
-        //given
-        User user = createUserAndSave(TEST_EMAIL);
-        UserInfo userInfo = UserMapper.toUserInfo(user);
-
-        LocalTime startTime = LocalTime.of(7, 0);
-        LocalTime endTime = LocalTime.of(9, 0);
-        Schedule schedule = createAndSaveSchedule(user, TEST_TITLE, TEST_CONTENT, startTime, endTime, TEST_DATE);
-
+    public void update_스케줄수정() {
         //when
         String updateTitle = "update_title";
         String updateContent = "update_content";
@@ -178,92 +128,11 @@ class ScheduleServiceTest {
         //then
         assertEquals(schedule.getTitle(), updateTitle);
         assertEquals(schedule.getContent(), updateContent);
-        assertEquals(schedule.getStartTime(), startTime);
-        assertEquals(schedule.getEndTime(), endTime);
     }
 
     @Test
-    @DisplayName("실패: 스케줄 수정시 endTime < startTime")
-    public void updateError() {
+    public void update_스케줄수정시_endTime이startTime보다작을때() {
         //given
-        User user = createUserAndSave(TEST_EMAIL);
-
-        LocalTime startTime = LocalTime.of(7, 0);
-        LocalTime endTime = LocalTime.of(9, 0);
-        Schedule schedule = createAndSaveSchedule(user, TEST_TITLE, TEST_CONTENT, startTime, endTime, TEST_DATE);
-
-        assertEquals(startTime, schedule.getStartTime());
-        assertEquals(endTime, schedule.getEndTime());
-        //when
-        String updateTitle = "update_title";
-        String updateContent = "update_content";
-
-        //then
-        assertThrows(IllegalArgumentException.class, () -> schedule.update(updateTitle, updateContent, LocalTime.of(10, 0), LocalTime.of(9, 10)));
+        assertThrows(IllegalArgumentException.class, () -> schedule.update(null, null, LocalTime.of(10, 0), LocalTime.of(9, 10)));
     }
-
-    @Test
-    @DisplayName("성공: 스케줄 상태변화")
-    public void statusTest() {
-        //given
-        User user = createUserAndSave(TEST_EMAIL);
-
-        LocalTime startTime = LocalTime.of(7, 0);
-        LocalTime endTime = LocalTime.of(9, 0);
-        Schedule schedule = createAndSaveSchedule(user, TEST_TITLE, TEST_CONTENT, startTime, endTime, TEST_DATE);
-
-        ScheduleStatus status = ScheduleStatus.POSSIBLE;
-        scheduleService.updateScheduleStatus(schedule.getId(), status);
-
-        Schedule result = scheduleRepository.findById(schedule.getId())
-                .orElseThrow(() -> new BusinessException(ErrorCode.SCHEDULE_NOT_FOUND));
-
-        assertEquals(ScheduleStatus.POSSIBLE, result.getStatus());
-    }
-
-    @Test
-    @DisplayName("성공: 스케줄을 생성할때 겹치는 스케줄 가져오기")
-    public void overlapSchedule(){
-        User user=createUserAndSave("email");
-
-        LocalTime startTime = LocalTime.of(7, 0);
-        LocalTime endTime = LocalTime.of(9, 0);
-        createAndSaveSchedule(user, TEST_TITLE, TEST_CONTENT, startTime, endTime, TEST_DATE);
-
-        LocalTime startTime2 = LocalTime.of(9, 0);
-        LocalTime endTime2 = LocalTime.of(11, 0);
-        createAndSaveSchedule(user, TEST_TITLE, TEST_CONTENT, startTime2, endTime2, TEST_DATE);
-
-        LocalTime startTime3 = LocalTime.of(9, 0);
-        LocalTime endTime3 = LocalTime.of(9, 30);
-        createAndSaveSchedule(user, TEST_TITLE, TEST_CONTENT, startTime3, endTime3, TEST_DATE);
-
-        List<Schedule> result = scheduleRepositoryCustom.findOverlapSchedules(user.getId(), TEST_DATE, LocalTime.of(8, 0), LocalTime.of(10, 0));
-        assertThat(result).isNotEmpty();
-        assertEquals(3,result.size());
-
-
-    }
-
-    private User createUserAndSave(String email) {
-        User user = User.builder()
-                .email(email)
-                .role(Role.USER)
-                .userCode("#CODE")
-                .username("username").build();
-        return userRepository.save(user);
-    }
-
-    private Schedule createAndSaveSchedule(User user, String title, String content, LocalTime startTime, LocalTime endTime, LocalDate date) {
-        Schedule schedule = Schedule.builder()
-                .user(user)
-                .scheduleDate(date)
-                .title(title)
-                .content(content)
-                .startTime(startTime)
-                .endTime(endTime)
-                .build();
-        return scheduleRepository.save(schedule);
-    }
-
 }
