ffdcc18c002a4f9a9fefa6fdcb0e0cbae00c00ee
SangWoon123 <alrax3574@naver.com>
2024-06-09 14:04:20 +0900

    fix: 스케줄 dto valid 적용

--- src/main/java/com/tukorea/planding/domain/schedule/dto/request/GroupScheduleRequest.java
+++ src/main/java/com/tukorea/planding/domain/schedule/dto/request/GroupScheduleRequest.java
@@ -1,15 +1,19 @@
 package com.tukorea.planding.domain.schedule.dto.request;
 
+import com.tukorea.planding.global.valid.schedule.ValidScheduleTime;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Builder
+@ValidScheduleTime
 public record GroupScheduleRequest(
         String userCode,
         String title,
         String content,
+        @NotNull(message = "스케줄 날짜를 입력해 주세요.")
         LocalDate scheduleDate,
         Integer startTime,
         Integer endTime
--- src/main/java/com/tukorea/planding/domain/schedule/dto/request/PersonalScheduleRequest.java
+++ src/main/java/com/tukorea/planding/domain/schedule/dto/request/PersonalScheduleRequest.java
@@ -1,13 +1,17 @@
 package com.tukorea.planding.domain.schedule.dto.request;
 
+import com.tukorea.planding.global.valid.schedule.ValidScheduleTime;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 
 import java.time.LocalDate;
 
 @Builder
+@ValidScheduleTime
 public record PersonalScheduleRequest(
         String title,
         String content,
+        @NotNull(message = "스케줄 날짜를 입력해 주세요.")
         LocalDate scheduleDate,
         Integer startTime,
         Integer endTime
